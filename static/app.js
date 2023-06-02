890c2f5ebf987be84fe5db88eb3beb79f3cf51a6
Ali Eren Öztürk <oztalieren@gmail.com>
2023-06-03 00:20:03 +0300

    add vuejs and connect api to ui

--- static/app.js
+++ static/app.js
@@ -1,49 +1,59 @@
-function update(data) {
-    // Clear the table body
-    const tableBody = document.getElementById('table-body');
-    tableBody.innerHTML = '';
+new Vue({
+  el: '#app',
+  data: {
+    stats: {},
+    sources: [],
+    logs: [],
+    currentPage: 1, // Current page number
+    totalPages: 1,
+  },
+  mounted() {
+    // Fetch initial log data when the app is mounted
+    this.fetchSources();
+  },
+  methods: {
+    fetchSources() {
+      fetch('/api/source')
+        .then(response => response.json())
+        .then(data => {
+          // Update the log data and metrics
+          this.sources = data;
+        })
+        .catch(error => {
+          console.error('Error fetching log data:', error);
+        });
 
-    // Populate the table with the fetched data
-    data._success_requests.forEach(item => {
-    const row = document.createElement('tr');
-    const url = document.createElement('td');
-    const timestamp = document.createElement('td');
-    const method = document.createElement('td');
-    const duration = document.createElement('td');
-    const response_status_code = document.createElement('td');
-
-    response_status_code.textContent = item.response_status_code;
-    duration.textContent = item.duration;
-    url.textContent = item.request_url;
-    method.textContent = item.request_method;
-    timestamp.textContent = item.timestamp;
-
-    row.appendChild(url);
-    row.appendChild(timestamp);
-    row.appendChild(method);
-    row.appendChild(duration);
-    row.appendChild(response_status_code);
-    tableBody.appendChild(row);
-    });
-
-    setTimeout(fetchData, 10000)
-}
-
-function fetchData() {
-    const levelFilter = document.getElementById('levelFilter');
-    const levelValue = levelFilter.value;
-
-    // Make a request to fetch data from the API
-    fetch(`api/log/stats?filter=${levelValue}`)
-      .then(response => response.json())
-      .then(data => {
-        update(data)
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-      });
-      
+    },
+    fetchLogsBySource(sourceId) {
+      // Make a request to the API endpoint
+      fetch(`/api/source/${sourceId}/logs?page=${this.currentPage}`)
+        .then(response => response.json())
+        .then(data => {
+          // Update the log data and metrics
+          this.logs = data.items;
+          this.totalPages = data.total_pages;
+          this.currentPage = data.current_page;
+        })
+        .then(this.fetchStatsBySource(sourceId))
+        .catch(error => {
+          console.error('Error fetching log data:', error);
+        });
+    },
+    fetchStatsBySource(sourceId) {
+      // Make a request to the API endpoint
+      fetch(`/api/source/${sourceId}/stats`)
+        .then(response => response.json())
+        .then(data => {
+          // Update the log data and metrics
+          this.stats = data;
+        })
+        .catch(error => {
+          console.error('Error fetching stats data:', error);
+        });
+    },
+    setCurrentPage(page) {
+      this.currentPage = page;
+      this.fetchLogs(); // Fetch logs for the selected page
+    },
   }
-
-  fetchData();
-  
+});
